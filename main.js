c2593a28248af606c98e35ed56c6c16da8d8853c
ninavdares <derkampf@outlook.com>
2016-07-15 12:31:13 -0400

    updated time

--- main.js
+++ main.js
@@ -61,6 +61,17 @@ var mainState = {
       pipe.outOfBoundsKill = true;
     },
 
+    addRowOfPipes: function() {
+        // Randomly pick a number between 1 and 5
+        // This will be the hole position
+        var hole = Math.floor(Math.random() * 5) + 1;
+
+        // Add the 6 pipes
+        // With one big hole at position 'hole' and 'hole + 1'
+        for (var i = 0; i < 8; i++)
+            if (i != hole && i != hole + 1)
+                this.addOnePipe(400, i * 60 + 10);
+    },
 
 
 }
